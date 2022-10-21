0089868f88c0e3b8cca091ccc441980805bdb6df
baizhi958216 <1475289190@qq.com>
2022-10-21 21:45:50 +0800

    Add more size button
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="header">
     <n-gradient-text type="info"> ICON Generator </n-gradient-text>
   </div>
+
   <div class="container">
     <div class="upload">
       <n-upload directory-dnd :max="1" accept=".png" :on-change="aupload">
@@ -19,13 +20,19 @@
     </div>
 
     <div class="genclick" v-if="src">
-      <n-button type="info" ghost @click="gen"> 生成 </n-button>
+      <n-button type="info" round ghost @click="gen16"> 16 x 16 </n-button>
+      <n-button type="info" round ghost @click="gen32"> 32 x 32 </n-button>
+      <n-button type="info" round ghost @click="gen64"> 64 x 64 </n-button>
+      <n-button type="info" round ghost @click="gen128"> 128 x 128 </n-button>
+      <n-button type="info" round ghost @click="gen256"> 256 x 256 </n-button>
+      <n-button type="info" round ghost @click="gen512"> 512 x 512 </n-button>
     </div>
 
     <div class="genview" v-if="src">
-      <n-image width="300" :src="imgblobview" />
+      <n-image :width="iwidth" :height="iheight" :src="imgblobview" />
     </div>
   </div>
+
   <div class="footer">
     <n-card hoverable>
       <n-gradient-text :size="14" type="info">
@@ -49,14 +56,37 @@ import {
 } from 'naive-ui'
 import { ArchiveOutline as ArchiveIcon } from '@vicons/ionicons5'
 const src = ref('')
+const iwidth = ref(128)
+const iheight = ref(128)
 const aupload = (e: any) => {
   src.value = window.URL.createObjectURL(e.file.file)
 }
 const imgblobview = computed(() => {
   return src.value
 })
-const gen = () => {
-  console.log(src.value)
+const gen16 = () => {
+  iwidth.value = 16
+  iheight.value = 16
+}
+const gen32 = () => {
+  iwidth.value = 32
+  iheight.value = 32
+}
+const gen64 = () => {
+  iwidth.value = 64
+  iheight.value = 64
+}
+const gen128 = () => {
+  iwidth.value = 128
+  iheight.value = 128
+}
+const gen256 = () => {
+  iwidth.value = 256
+  iheight.value = 256
+}
+const gen512 = () => {
+  iwidth.value = 512
+  iheight.value = 512
 }
 </script>
 
@@ -68,13 +98,20 @@ const gen = () => {
 }
 .container {
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
+  flex-wrap: no-wrap;
+  height: 100vh;
+  justify-content: space-around;
+  overflow: scroll;
+}
+.genclick {
+  display: flex;
   flex-wrap: wrap;
 }
 .footer {
   text-align: center;
-  position: absolute;
+  position: fixed;
   top: 100%;
   transform: translateY(-100%);
   width: 100vw;
